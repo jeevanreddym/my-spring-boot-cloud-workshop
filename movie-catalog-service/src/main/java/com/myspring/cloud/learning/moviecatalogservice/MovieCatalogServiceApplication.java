d3c1713a5493ae8553933fb836565f6ea2dab044
jeevanreddym <jeevan.rocks@gmail.com>
2022-07-05 12:57:15 -0400

    Add files via upload

---
+++ movie-catalog-service/src/main/java/com/myspring/cloud/learning/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,26 @@
+package com.myspring.cloud.learning.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MovieCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogServiceApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000); // 3 secs timeout.
+        return new RestTemplate(clientHttpRequestFactory);
+    }
+
+}
---
+++ movie-catalog-service/src/main/java/com/myspring/cloud/learning/moviecatalogservice/model/CatalogItem.java
@@ -0,0 +1,17 @@
+package com.myspring.cloud.learning.moviecatalogservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CatalogItem {
+
+    private String name;
+    private String desc;
+    private int rating;
+
+}
