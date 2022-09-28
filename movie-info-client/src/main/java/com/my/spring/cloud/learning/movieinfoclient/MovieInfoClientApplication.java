04fedb837a6cf9164453397e6a44eff7c1bfe5e3
jeevan reddy madhugiripally <jeevan.rocks@gmail.com>
2022-09-28 12:16:58 -0400

    Add files via upload

---
+++ movie-info-client/src/main/java/com/my/spring/cloud/learning/movieinfoclient/MovieInfoClientApplication.java
@@ -0,0 +1,13 @@
+package com.my.spring.cloud.learning.movieinfoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoClientApplication.class, args);
+	}
+
+}
